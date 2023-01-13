4b831a23f2e689f5a25485d546d2f20dc69bfe3b
MJ <mirjanadukicc@gmail.com>
2023-01-12 20:41:29 -0800

    chads helper package

--- player/player.js
+++ player/player.js
@@ -1,15 +1,15 @@
-let playerState = "idle";
+let playerState = 'idle';
 const menu = document.querySelector('#animations');
-menu.addEventListener('change', function(event){
-    playerState = event.target.value;
-})
+menu.addEventListener('change', function (event) {
+  playerState = event.target.value;
+});
 
 const canvas1 = document.getElementById('canvas1');
 const ctx = canvas1.getContext('2d');
-const canvasWidth = canvas1.width = innerWidth;
-const canvasHeight = canvas1.height = innerHeight;
+const canvasWidth = (canvas1.width = innerWidth);
+const canvasHeight = (canvas1.height = innerHeight);
 
-const playerSprite = new Image()
+const playerSprite = new Image();
 playerSprite.src = 'sheets/redhood-spritesheet.png';
 const spriteWidth = 32;
 const spriteHeight = 32;
@@ -21,105 +21,120 @@ let vxr = 0;
 let vyu = 0;
 let vyd = 0;
 let frameX = 0;
-let frameY = 0;
+// let frameY = 0;
 let gameFrame = 0;
 const framePersistence = 7;
 const spriteAnimations = [];
 const animationStates = [
-    {
-        name: 'idle',
-        frames: 2,
-    },
-    {
-        name: 'idle2',
-        frames: 2,
-    },
-    {
-        name: 'walking',
-        frames: 4,
-    },
-    {
-        name: 'running',
-        frames: 8,
-    },
-    {
-        name: 'crouching',
-        frames: 6,
-    },
-    {
-        name: 'jumping',
-        frames: 8,
-    },
-    {
-        name: 'banished',
-        frames: 6,
-    },
-    {
-        name: 'defeated',
-        frames: 8,
-    },
-    {
-        name: 'attack',
-        frames: 8,
-    }
-]
+  {
+    name: 'idle',
+    frames: 2
+  },
+  {
+    name: 'idle2',
+    frames: 2
+  },
+  {
+    name: 'walking',
+    frames: 4
+  },
+  {
+    name: 'running',
+    frames: 8
+  },
+  {
+    name: 'crouching',
+    frames: 6
+  },
+  {
+    name: 'jumping',
+    frames: 8
+  },
+  {
+    name: 'banished',
+    frames: 6
+  },
+  {
+    name: 'defeated',
+    frames: 8
+  },
+  {
+    name: 'attack',
+    frames: 8
+  }
+];
 
 animationStates.forEach((state, index) => {
-    let frames = {
-        loc: [],
-    }
-    for (let i = 0; i <state.frames; i++){
-        let positionX = i * spriteWidth;
-        let positionY = index * spriteHeight;
-        frames.loc.push({x: positionX, y: positionY});
-    }
-    spriteAnimations[state.name] = frames;
+  let frames = {
+    loc: []
+  };
+  for (let i = 0; i < state.frames; i++) {
+    let positionX = i * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
 });
-
 
 function animate() {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    x += vxl;
-    x += vxr;
-    y += vyu;
-    y += vyd;
-    let position = Math.floor(gameFrame/framePersistence) % spriteAnimations[playerState].loc.length;
-    frameX = spriteWidth * position;
-    let frameY = spriteAnimations[playerState].loc[position].y;
-    // if (x < 0) {    
-    //     flipHorizontally(playerSprite, spriteWidth, spriteHeight, x, y);
-    // } else {
-    //     ctx.drawImage(playerSprite, frameX, frameY, spriteWidth, spriteHeight, x, y, spriteWidth, spriteHeight);
-    // }
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  x += vxl;
+  x += vxr;
+  y += vyu;
+  y += vyd;
+  let position =
+    Math.floor(gameFrame / framePersistence) %
+    spriteAnimations[playerState].loc.length;
+  frameX = spriteWidth * position;
+  let frameY = spriteAnimations[playerState].loc[position].y;
+  // if (x < 0) {
+  //     flipHorizontally(playerSprite, spriteWidth, spriteHeight, x, y);
+  // } else {
+  //     ctx.drawImage(playerSprite, frameX, frameY, spriteWidth, spriteHeight, x, y, spriteWidth, spriteHeight);
+  // }
 
-    ctx.drawImage(playerSprite, frameX, frameY, spriteWidth, spriteHeight, x, y, spriteWidth, spriteHeight);
+  ctx.drawImage(
+    playerSprite,
+    frameX,
+    frameY,
+    spriteWidth,
+    spriteHeight,
+    x,
+    y,
+    spriteWidth,
+    spriteHeight
+  );
 
-    gameFrame++;
-    requestAnimationFrame(animate);
-};
-
-function flipHorizontally(playerSprite, spriteWidth, spriteHeight, x, y){
-    let position = Math.floor(gameFrame/framePersistence) % spriteAnimations[playerState].loc.length;
-    frameX = spriteWidth * position;
-    let frameY = spriteAnimations[playerState].loc[position].y;
-
-    ctx.translate(x+spriteWidth, y);
-
-    ctx.scale(-1,1);
-    
-    ctx.drawImage(playerSprite, frameX, frameY, spriteWidth, spriteHeight, x, y, spriteWidth, spriteHeight);
-    
-    ctx.setTransform(1,0,0,1,0,0);
+  gameFrame++;
+  requestAnimationFrame(animate);
 }
 
-addEventListener("keydown", function(event){
-    console.log(event.code)
-    if (event.code === 'ShiftLeft') {playerState = "running"};
+// function flipHorizontally(playerSprite, spriteWidth, spriteHeight, x, y){
+//     let position = Math.floor(gameFrame/framePersistence) % spriteAnimations[playerState].loc.length;
+//     frameX = spriteWidth * position;
+//     let frameY = spriteAnimations[playerState].loc[position].y;
+
+//     ctx.translate(x+spriteWidth, y);
+
+//     ctx.scale(-1,1);
+
+//     ctx.drawImage(playerSprite, frameX, frameY, spriteWidth, spriteHeight, x, y, spriteWidth, spriteHeight);
+
+//     ctx.setTransform(1,0,0,1,0,0);
+// }
+
+addEventListener('keydown', function (event) {
+  console.log(event.code);
+  if (event.code === 'ShiftLeft') {
+    playerState = 'running';
+  }
 });
 
-addEventListener("keyup", function(event){
-    console.log(event.code)
-    if (event.code === 'ShiftLeft') {playerState = "idle"};
-})
+addEventListener('keyup', function (event) {
+  console.log(event.code);
+  if (event.code === 'ShiftLeft') {
+    playerState = 'idle';
+  }
+});
 
 animate();
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const sequelize = require('./config/connections');
 
 
-app.use(express.json())
+app.use(express.json());
 
-const subscribersRouter = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouter)
+const subscribersRouter = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouter);
+sequelize.sync().then(() => app.listen(3001, () => console.log('Server Started')));
 
-app.listen(3001, () => console.log('Server Started'))
 
 
