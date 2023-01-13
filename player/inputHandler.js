1121c3606156d2be107d2c99ffd34bc48d78c876
Brandon <brandonkly@ucla.edu>
2023-01-12 16:02:24 -0800

    movable player sprite, shift to change animation, depreciated code removed

--- player/inputHandler.js
+++ player/inputHandler.js
@@ -6,7 +6,7 @@ addEventListener("keydown", function(event){
     if (event.code === 'KeyA') {vxl = -5};
     if (event.code === 'KeyS') {vyd = 5};
     if (event.code === 'KeyD') {vxr = 5};
-})
+});
 
 addEventListener("keyup", function(event){
     console.log(event.code)
@@ -14,20 +14,4 @@ addEventListener("keyup", function(event){
     if (event.code === 'KeyA') {vxl = 0};
     if (event.code === 'KeyS') {vyd = 0};
     if (event.code === 'KeyD') {vxr = 0};
-})
-
-addEventListener("keydown", function(event){
-    console.log(event.code)
-    if (event.code === 'KeyW') {vy = -5};
-    if (event.code === 'KeyA') {vx = -5};
-    if (event.code === 'KeyS') {vy = 5};
-    if (event.code === 'KeyD') {vx = 5};
-})
-
-addEventListener("keyup", function(event){
-    console.log(event.code)
-    if (event.code === 'KeyW') {vy = 0};
-    if (event.code === 'KeyA') {vx = 0};
-    if (event.code === 'KeyS') {vy = 0};
-    if (event.code === 'KeyD') {vx = 0};
 })
--- player/player.js
+++ player/player.js
@@ -1,5 +1,3 @@
-// https://www.youtube.com/watch?v=GFO_txvwK_c&t=1s&ab_channel=freeCodeCamp.org
-
 let playerState = "idle";
 const menu = document.querySelector('#animations');
 menu.addEventListener('change', function(event){
@@ -8,14 +6,20 @@ menu.addEventListener('change', function(event){
 
 const canvas1 = document.getElementById('canvas1');
 const ctx = canvas1.getContext('2d');
-const canvasWidth = canvas1.width = 32;
-const canvasHeight = canvas1.height = 32;
+const canvasWidth = canvas1.width = innerWidth;
+const canvasHeight = canvas1.height = innerHeight;
 
 const playerSprite = new Image()
 playerSprite.src = 'sheets/redhood-spritesheet.png';
 const spriteWidth = 32;
 const spriteHeight = 32;
 
+let x = 0;
+let y = 0;
+let vxl = 0;
+let vxr = 0;
+let vyu = 0;
+let vyd = 0;
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
@@ -75,13 +79,47 @@ animationStates.forEach((state, index) => {
 
 function animate() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    x += vxl;
+    x += vxr;
+    y += vyu;
+    y += vyd;
     let position = Math.floor(gameFrame/framePersistence) % spriteAnimations[playerState].loc.length;
     frameX = spriteWidth * position;
     let frameY = spriteAnimations[playerState].loc[position].y;
-    ctx.drawImage(playerSprite, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    // if (x < 0) {    
+    //     flipHorizontally(playerSprite, spriteWidth, spriteHeight, x, y);
+    // } else {
+    //     ctx.drawImage(playerSprite, frameX, frameY, spriteWidth, spriteHeight, x, y, spriteWidth, spriteHeight);
+    // }
+
+    ctx.drawImage(playerSprite, frameX, frameY, spriteWidth, spriteHeight, x, y, spriteWidth, spriteHeight);
 
     gameFrame++;
     requestAnimationFrame(animate);
 };
+
+function flipHorizontally(playerSprite, spriteWidth, spriteHeight, x, y){
+    let position = Math.floor(gameFrame/framePersistence) % spriteAnimations[playerState].loc.length;
+    frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+
+    ctx.translate(x+spriteWidth, y);
+
+    ctx.scale(-1,1);
+    
+    ctx.drawImage(playerSprite, frameX, frameY, spriteWidth, spriteHeight, x, y, spriteWidth, spriteHeight);
+    
+    ctx.setTransform(1,0,0,1,0,0);
+}
+
+addEventListener("keydown", function(event){
+    console.log(event.code)
+    if (event.code === 'ShiftLeft') {playerState = "running"};
+});
+
+addEventListener("keyup", function(event){
+    console.log(event.code)
+    if (event.code === 'ShiftLeft') {playerState = "idle"};
+})
 
 animate();
