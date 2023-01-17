443fc0a3cc205b12d377f245dd93ae48223d53ea
chrispoturalski <chrispotural@gmail.com>
2023-01-17 14:55:24 -0800

    added schema and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO users (username, pass)
+VALUES  ("", "");
+
+INSERT INTO characters (characters, health, inventory)
+VALUES  ("Main", 100, FALSE);
+        ("Secondary", 100, FALSE );
+        ("Leveled Up", 125, TRUE );
+
+INSERT INTO villains (villain, health, inventory)
+VALUES  ("One", 75, TRUE);
+        ("Two", 100, TRUE );
+        ("Three", 125, TRUE );
+        ("Four", 150, TRUE);
+
+INSERT INTO inventory (item, damage, equipped)
+VALUES  ("Villain Sword", 10, TRUE);
+        ("Bat", 75, FALSE );
+        ("Sword", 100, FALSE );
+        ("Power-up", 150, FALSE);
+INSERT INTO levels (levels)
+VALUES  (1);
+        (2);
+        (3);
+        (4);
