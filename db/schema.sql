6738623695e14622ef5513cfdb5614548ddd1c38
chrispoturalski <chrispotural@gmail.com>
2023-01-18 15:00:02 -0800

    redid the database to create a leaderboards that will track the users score and compare against multiple players

--- db/schema.sql
+++ db/schema.sql
@@ -1,51 +1,28 @@
-DROP DATABASE IF EXISTS user_db;
-CREATE DATABASE user_db;
 
-USE user_db;
+DROP DATABASE IF EXISTS leaderboards;
+CREATE DATABASE leaderboards;
+
+USE leaderboards;
 
 CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    pass VARCHAR(30) NOT NULL,
-)
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    pass VARCHAR(30) NOT NULL
+);
 
-CREATE TABLE leaderboards (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    score INT,
-    levels INT,
-    REFERENCES levels(levels) && users(username),
-    ORDER BY score DESC,
-)
+CREATE TABLE scores (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    score INT NOT NULL
+    REFERENCES users(username)
+);
 
-CREATE TABLE characters (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user VARCHAR(30) NOT NULL,
-    lives INT,
-    inventory BOOLEAN
-    FOREIGN KEY (user)
-    REFERENCES users(id)
-)
 
-CREATE TABLE villains (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    villain VARCHAR(30)NOT NULL,
-    health INT,
-    levels-found INT,
-    FOREIGN KEY (levels-found),
-    REFERENCES levels(levels)
-)
 
-CREATE TABLE inventory (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    item VARCHAR(30),
-    damage INT,
-    equipped BOOLEAN,
-    FOREIGN KEY (equipped)
-    REFERENCES characters(id)
-)
 
-CREATE TABLE levels (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    levels INT
-)
+
+
+
+
+
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,51 @@
-INSERT INTO users (username, pass)
-VALUES  ("Chris", "ABC1234");
-        ("Brandon", "BCA4312")
-        ("Mirjana", "CBA2134")
-        ("Logan", "BAC1423")
+use leaderboards
 
-INSERT INTO leaderboards (username, score, levels)
-VALUES  ("Chris", 180, 1);
-        ("Brandon", 500, 1);
-        ("Mirjana", 230, 1);
-        ("Brandon", 610, 1);
+INSERT INTO users
+    (username, pass)
+VALUES
+    ('Chris', 'ABC1234'),
+    ('Brandon', 'ACB1324'),
+    ('Mirjana', 'BCA2341'),
+    ('Logan', 'CCA2233');
+
+INSERT INTO scores
+    (username, score)
+VALUES
+    ('Chris', 110),
+    ('Brandon', 1110),
+    ('Mirjana', 120),
+    ('Logan', 150);
+
+-- INSERT INTO leaderboards (username, pass)
+-- VALUES  ("Chris", "ABC1234");
+--         ("Brandon", "BCA4312")
+--         ("Mirjana", "CBA2134")
+--         ("Logan", "BAC1423")
+
+-- INSERT INTO leaderboards (username, score, levels)
+-- VALUES  ("Chris", 180, 1);
+--         ("Brandon", 500, 1);
+--         ("Mirjana", 230, 1);
+--         ("Brandon", 610, 1);
 
 
-INSERT INTO characters (characters, health, inventory)
-VALUES  ("Main", 100, FALSE);
-        ("Leveled Up", 125, TRUE );
+-- INSERT INTO characters (characters, health, inventory)
+-- VALUES  ("Main", 100, FALSE);
+--         ("Leveled Up", 125, TRUE );
 
-INSERT INTO villains (villain, health, levels-found)
-VALUES  ("Ghost", 75, 1);
-        ("Ghost2", 100, 2 );
-        ("Ghost3", 125, 3 );
-        ("Ghost4", 150, 4);
+-- INSERT INTO villains (villain, health, levels-found)
+-- VALUES  ("Ghost", 75, 1);
+--         ("Ghost2", 100, 2 );
+--         ("Ghost3", 125, 3 );
+--         ("Ghost4", 150, 4);
 
-INSERT INTO inventory (item, damage, equipped)
-VALUES  ("Villain Sword", 10, TRUE);
-        ("Bat", 75, FALSE );
-        ("Sword", 100, FALSE );
-        ("Power-up", 150, FALSE);
-INSERT INTO levels (levels)
-VALUES  (1);
-        (2);
-        (3);
-        (4);
+-- INSERT INTO inventory (item, damage, equipped)
+-- VALUES  ("Villain Sword", 10, TRUE);
+--         ("Bat", 75, FALSE );
+--         ("Sword", 100, FALSE );
+--         ("Power-up", 150, FALSE);
+-- INSERT INTO levels (levels)
+-- VALUES  (1);
+--         (2);
+--         (3);
+--         (4);
