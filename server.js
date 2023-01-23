cd1e3866cdbd462fd3512b026e861ee2efc0b272
chrispoturalski <chrispotural@gmail.com>
2023-01-23 11:35:03 -0800

    added websocket server, will need to have Nick go over tonight as I am receiving some errors in the functions in the dependencies that I had to download

--- server.js
+++ server.js
@@ -3,7 +3,27 @@ const express = require('express')
 const app = express();
 const sequelize = require('./config/connections');
 const PORT = process.env.PORT || 3001;
+// reference: https://www.piesocket.com/blog/nodejs-websocket
+const WebSocket = require('ws');
 
+const wss = new WebSocket.Server({ server: 3001 });
+
+// creating connection with websocket
+wss.on("connection", ws => {
+  console.log("new client connect");
+// sending message
+  ws.on("message", data => {
+    console.log(`Client has sent us: ${data}`)
+  });
+  // handling when client disconnects
+  ws.on("close", () => {
+    console.log("the client has disconnected");
+  });
+  ws.onerror = function () {
+    console.log("Some error occured")
+  }
+});
+console.log("The websocket server is running on port 3001")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
