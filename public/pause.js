488f6ad55afb44f2f25e22d6ffa946e165f86205
Brandon <brandonkly@ucla.edu>
2023-01-27 18:31:24 -0800

    pause is fully functional

--- public/pause.js
+++ public/pause.js
@@ -1,5 +1,19 @@
 class PauseScene extends Phaser.Scene {
     constructor() {
-        super('GameScene1');
-    }
+        super('PauseScene');
+        this.cursors = undefined;
+    };
+    preload() {
+
+    };
+    create() {
+        this.cursors = this.input.keyboard.addKeys('W,S,A,D, SPACE, P, ESC');
+        console.log('logging cursors', this.cursors);
+    };
+    update() {
+        if (this.cursors.P.isDown) {
+            this.scene.resume('GameScene1');
+            this.scene.stop('PauseScene')
+            } 
+    };
 }
